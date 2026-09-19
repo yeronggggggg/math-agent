75f0788b19ee8b5b7e0325987bb9eda69faa1a3f
yeronggggggg <purplecat_2015@163.com>
2026-09-19 18:10:14 +0800

    增加langgraph agent

--- agent.py
+++ agent.py
@@ -1,5 +1,4 @@
-from langchain_core.tools import tool
-from langchain_core.messages import SystemMessage, HumanMessage, ToolMessage
+from langchain_core.messages import SystemMessage, ToolMessage
 import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
@@ -15,37 +14,106 @@ from tools import calculator, solve_equation, differentiate, integrate_function,
 
 tools = [calculator, solve_equation, differentiate, integrate_function, matrix_calculator]
 tool_map={tool.name: tool for tool in tools}
-max_steps = 10
+model_with_tools = model.bind_tools(tools)
 
-def ask_math_question(question:str):
-    message=[
-        SystemMessage(content="你是一个数学专家，擅长使用计算器、方程求解器、求导、求积分和矩阵运算工具。请根据用户的输入选择合适的工具进行计算，并提供详细的解释。"),
-        HumanMessage(content=question),
-    ]
-    model_with_tools = model.bind_tools(tools)
-    response = model_with_tools.invoke(message)
-    if not response.tool_calls:
-        return response.content
-    message.append(response)
-    while response.tool_calls != [] :
-        max_steps -= 1
-        if max_steps <= 0:
-            return "计算步骤过多，可能存在无限循环，请检查输入。"
-        for tool_call in response.tool_calls:
-            tool_name = tool_call['name']
-            if tool_name in tool_map:
-                select_tool = tool_map.get(tool_name)
-                tool_message = select_tool.invoke(tool_call)
-                message.append(tool_message)
+# def ask_math_question(question:str):
+#     max_steps = 10
+#     message=[
+#         SystemMessage(content="你是一个数学专家，擅长使用计算器、方程求解器、求导、求积分和矩阵运算工具。请根据用户的输入选择合适的工具进行计算，并提供详细的解释。"),
+#         HumanMessage(content=question),
+#     ]
+#     response = model_with_tools.invoke(message)
+#     if not response.tool_calls:
+#         return response.content
+#     message.append(response)
+#     while response.tool_calls != [] :
+#         max_steps -= 1
+#         last_error=None
+#         if max_steps <= 0:
+#             if last_error is not None:
+#                 return last_error
+#             else:
+#                 return "调用次数过多，可能存在无限循环情况，请重新输入。"
+#         for tool_call in response.tool_calls:
+#             tool_name = tool_call['name']
+#             if tool_name in tool_map:
+#                 try:
+#                     select_tool = tool_map.get(tool_name)
+#                     tool_message = select_tool.invoke(tool_call)
+                    
+#                 except Exception as e:
+#                     tool_message=ToolMessage(
+#                     content=f"工具 {tool_name} 执行失败:{e}",
+#                     tool_call_id=tool_call["id"],
+#                     name=tool_name
+#                 )
+#                     last_error=(f"工具 {tool_name} 执行失败:{e}")
+            
+#             else:
+#                 tool_message=ToolMessage(
+#                     content=f"工具 {tool_name} 未找到。",
+#                     tool_call_id=tool_call["id"],
+#                     name=tool_name
+#                 )
+#                 last_error=(f"工具 {tool_name} 未找到。")
+#             message.append(tool_message)
+#         mid_response = model_with_tools.invoke(message)
+#         message.append(mid_response)
+#         response = mid_response
+
+#     return response.content
+
+
+
+from langgraph.graph import StateGraph, END
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
+from typing import TypedDict, Annotated
+import operator
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], operator.add]
+
+
+class Agent:
+
+    def __init__(self,model,tools,system=""):
+        self.system =system
+        graph=StateGraph(AgentState)
+        graph.add_node("llm",self.call_openai)
+        graph.add_node("action",self.take_action)
+        graph.add_conditional_edges(
+            "llm",
+            self.exist_action,
+            {True:"action",False:END}
+        )
+        graph.add_edge("action","llm")
+        graph.set_entry_point("llm")
+        self.graph=graph.compile()
+        self.tools={t.name: t for t in tools}
+        self.model=model.bind_tools(tools)
+
+    def call_openai(self,state:AgentState):
+        messages=state["messages"]
+        if self.system:
+            messages=[SystemMessage(content=self.system)]+messages
+        message=self.model.invoke(messages)
+        return{'messages':[message]}
+
+    def take_action(self,state:AgentState):
+        tool_calls=state["messages"][-1].tool_calls
+        results=[]
+        for t in tool_calls:
+            print(f"Calling:{t}")
+            if not t['name'] in self.tools:
+                print("工具不存在")
+                result="工具不存在"
             else:
-                tool_message=ToolMessage(
-                    content=f"工具 {tool_name} 未找到。"
-                    tool_call_id=tool_call["id"]
-                    name=tool_name
-                )
-                return f"工具 {tool_name} 未找到。"
-        mid_response = model_with_tools.invoke(message)
-        message.append(mid_response)
-        response = mid_response
+                result=self.tools[t["name"]].invoke(t["args"])
+            results.append (ToolMessage(tool_call_id=t["id"],name=t["name"],content=str(result)))
+        print("返回模型")
+        return {'messages':results}
 
-    return response.content
+    def exist_action(self,state:AgentState):
+        result = state["messages"][-1]
+        return len(result.tool_calls)>0
+
--- main.py
+++ main.py
@@ -1,7 +1,62 @@
-from agent import ask_math_question
+from agent import Agent
+from tools import calculator, solve_equation, differentiate, integrate_function, matrix_calculator
+from langchain_core.messages import HumanMessage
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+# model = ChatOpenAI(
+#     model="deepseek-flash",
+#     base_url="https://api.deepseek.com",
+#     api_key=os.getenv("DEEPSEEK_API_KEY")
+# )
+
+model = ChatOpenAI(
+    model="deepseek-flash",
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com",
+    temperature=0,
+    extra_body={
+        "thinking": {
+            "type": "disabled"
+        }
+    }
+)
+
+
+prompt=""""你是一个非常聪明的数学助手，使用工具来解决数学问题。\
+你可以进行多次调用\
+只能在确定工具的时候进行使用\
+请根据用户的输入选择合适的工具进行计算，并提供详细的解释。
+"""
+tools = [calculator, solve_equation, differentiate, integrate_function, matrix_calculator]
+#tools={tool.name: tool for tool in cal_tools}
+abot=Agent(model,tools,system=prompt)
+
+# while True:
+#     question = input("请输入数学问题（例如 'sin(pi / 2)的值'，'x + y = 5, x - y = 1'，'对 x 求导 x**2 + 3*x + 2'），退出请输入'exit'：")
+#     if question.lower() == 'exit':
+#         break
+#     print(ask_math_question(question))
+
+# from IPython.display import Image
+# image=Image(abot.graph.get_graph().draw_png())
+
+# from PIL import Image
+# from io import BytesIO
+# png_data = abot.graph.get_graph().draw_mermaid_png()
+# Image.open(BytesIO(png_data)).show()
 
 while True:
-    question = input("请输入数学问题（例如 'sin(pi / 2)的值'，'x + y = 5, x - y = 1'，'对 x 求导 x**2 + 3*x + 2'），退出请输入'exit'：")
+    question=input(f"请输入数学问题，退出请输入'exit'： ")
     if question.lower() == 'exit':
         break
-    print(ask_math_question(question))
+    messages = [HumanMessage(content=question)]
+    result = abot.graph.invoke({"messages": messages},config={"recursion_limit":20})
+    #print(f"最终回答 \n {result['messages'][-1].content}")
+
+    for event in abot.graph.stream({"messages": messages},config={"recursion_limit":20}, stream_mode="updates"):
+        node_name = next(iter(event))
+        print("经过节点：", node_name)
+        #print(event)
